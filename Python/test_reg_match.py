98ec687c09915db0ad00e91c5a5ae642680c0ba0
Michael Coughlin <30599857+MichaelCoughlinAN@users.noreply.github.com>
2019-12-02 10:32:23 -0600

    Update and rename test_reg_match.py to test_regex_match.py

--- Python/test_reg_match.py
+++
@@ -1,9 +0,0 @@
-import re
-regex = re.compile('app-(.*)-name.jar')
-l = ['app-4.1.118-name.jar', 'app-4.1.119-name.jar', 'just', 'a', 'test']
-matches = [string for string in l if re.search(regex, string)]
-
-if len(matches) > 0:
-    print 'There are matches'
-else:
-    print 'There are no'
