135650b0ef705031301578663fa4c59e16bb6b63
Michael Coughlin <30599857+MichaelCoughlinAN@users.noreply.github.com>
2019-12-02 10:29:33 -0600

    Update multicast_listener.py

--- Python/multicast_listener.py
+++ Python/multicast_listener.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Michael Coughlin
+# hiimmichael.com
+# 2019
+
 import socket
 import struct
 
@@ -9,11 +13,11 @@ MCAST_PORT = 4446
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
-                             # to MCAST_GRP, not all groups on MCAST_PORT
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 print 'running'
+
 while True:
   print 'hello' +  sock.recv(10240)
