1245e17505354b710fd1917eca15af4fbaff52e9
Michael Coughlin <30599857+MichaelCoughlinAN@users.noreply.github.com>
2019-12-02 10:32:40 -0600

    Update test_regex_match.py

--- Python/test_regex_match.py
+++ Python/test_regex_match.py
@@ -10,6 +10,6 @@ l = ['application-4.1.118-name.jar', 'application-4.1.119-name.jar', 'just', 'a'
 matches = [string for string in l if re.search(regex, string)]
 
 if len(matches) > 0:
-    print 'There are matches'
+    print 'There are matches!'
 else:
-    print 'There are no'
+    print 'There are no matches ...'
