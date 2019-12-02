82a9e14acfe5c2e727a731450e927d4465c00b39
MichaelCoughlinAN <Michael_J_CoughlinAN@graco.com>
2019-12-02 13:01:43 -0600

    removed unused files

--- C++/Binary_Node_Tree/NotFoundException.cpp
+++
@@ -1,9 +0,0 @@
-#include "NotFoundException.h"
-
-NotFoundException::NotFoundException(const string& message)
-  : logic_error(message.c_str() ) {
-}
-
-NotFoundException::~NotFoundException()
-  throw() {
-}
