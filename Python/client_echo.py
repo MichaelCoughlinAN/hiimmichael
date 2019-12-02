68d6b45c72cb97b1f503f87ee1ef5edce83fd9e5
Michael Coughlin <30599857+MichaelCoughlinAN@users.noreply.github.com>
2019-12-02 10:19:28 -0600

    Update client_echo.py

--- Python/client_echo.py
+++ Python/client_echo.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+# Michael Coughlin
+# hiimmichael.com
+# 2019
+
 import socket
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', 5001))
-#00bfff
+
 while 1:
     data, client = sock.recvfrom(256)
     sock.sendto("hello", client)
