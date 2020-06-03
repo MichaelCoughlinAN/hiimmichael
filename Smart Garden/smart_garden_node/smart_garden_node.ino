c9181ab70c53e027e7ff3c16cda699835d863b98
Michael Coughlin <30599857+MichaelCoughlinAN@users.noreply.github.com>
2020-06-03 15:44:22 -0500

    Update smart_garden_node.ino

--- Smart Garden/smart_garden_node/smart_garden_node.ino
+++ Smart Garden/smart_garden_node/smart_garden_node.ino
@@ -5,25 +5,24 @@
 
 const unsigned long SECOND = 1000;
 const unsigned long HOUR = 3600 * SECOND;
-
 const int sensorPin = A0; // Set analog pin for moisture sensor
 
 // Take soil moisture reading from analog sensor
 int getSoilMoisture() {
-  int sensorValue = 0;
-  sensorValue = analogRead(sensorPin);
-  return sensorValue;
+    int sensorValue = 0;
+    sensorValue = analogRead(sensorPin);
+    return sensorValue;
 }
 
-
 void setup() {
-  Serial.begin(115200);
-  delay(100);
+    Serial.begin(115200);
+    delay(100);
 }
  
 void loop() { 
-  int soilMoisture = getSoilMoisture();
+    int soilMoisture = getSoilMoisture();
     
-  // TODO: Send data to web server via HTTP or MQTT  
-  delay(1*HOUR);
+    // TODO: Send data to web server via HTTP or MQTT  
+ 
+    delay(1*HOUR);
 }
